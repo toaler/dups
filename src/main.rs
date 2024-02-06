c479c9c26200c54dacf22a3637c818405f4d1f9f
toaler <brian.toal@gmail.com>
2024-02-05 23:30:11 -0800

    Cleaned up cli processing

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,19 @@ mod util;
 mod resource_metadata;
 mod largest_files_vistor;
 mod directory_analyzer_visitor;
+mod turbo_tasker_cli_config;
 
+use crate::turbo_tasker_cli_config::{Command, TurboTaskerApp};
 use log::{debug, error, info};
-use std::{env, io};
+use std::io;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufReader};
 use std::path::{Path};
 use std::time::{Instant};
 use std::error::Error;
+use std::process::exit;
+use clap::{CommandFactory, Parser};
 use csv::{ReaderBuilder, WriterBuilder};
 use env_logger::Env;
 use crate::directory_analyzer_visitor::DirectoryAnalyzerVisitor;
@@ -33,78 +37,76 @@ fn main() -> Result<(), Box<dyn Error>> {
     // TODO add flag to enable fingerprinting
 
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).target(env_logger::Target::Stdout).init();
-    info!("Running Turbo Tasker (tt)!!!");
 
-    let root = process_args()?;
 
-    debug!("Register visitors:");
-    let mut scan_stats_visitor = ScanStatsVisitor::new();
-    let mut progress_visitor = ProgressVisitor::new();
-    let mut top_resources_visitor = Top50LargestResources::new();
-    let mut directory_analyzer_visitor = DirectoryAnalyzerVisitor::new();
+    let cli = TurboTaskerApp::parse();
+    match cli.cmd {
+        Some(Command::Disk { duplicate_detection: _, root, .. }) => {
+            info!("Running Turbo Tasker (tt)!!!");
+            debug!("Register visitors:");
 
-    let mut visitors: Vec<&mut dyn Visitable> = vec![
-        &mut progress_visitor,
-        &mut scan_stats_visitor,
-        &mut directory_analyzer_visitor,
-        &mut top_resources_visitor,
-    ];
+            let mut scan_stats_visitor = ScanStatsVisitor::new();
+            let mut progress_visitor = ProgressVisitor::new();
+            let mut top_resources_visitor = Top50LargestResources::new();
+            let mut directory_analyzer_visitor = DirectoryAnalyzerVisitor::new();
 
-    for v in &mut *visitors {
-        debug!("Visitor registered: {}", v.name());
-    }
+            let mut visitors: Vec<&mut dyn Visitable> = vec![
+                &mut progress_visitor,
+                &mut scan_stats_visitor,
+                &mut directory_analyzer_visitor,
+                &mut top_resources_visitor,
+            ];
 
-    let start_time = Instant::now();
-    let mut scanner = ResourceScanner::new();
-    let mut registry: HashMap<String, ResourceMetadata> = HashMap::new();
+            for v in &mut *visitors {
+                debug!("Visitor registered: {}", v.name());
+            }
 
-    if Path::new("output.csv").exists() {
-        info!("Incremental scan detected");
-        load_registry("output.csv", &mut registry)?;
+            let start_time = Instant::now();
+            let mut scanner = ResourceScanner::new();
+            let mut registry: HashMap<String, ResourceMetadata> = HashMap::new();
 
-        // Add root dir in case it's not known from previous scans
-        if !registry.contains_key(&root) {
-            // Create ResourceMetadata and insert into the registry
-            let p = Path::new(&root);
-            let m = ResourceMetadata::new(&root, p.is_dir(), p.is_symlink(), 0, 0, false);
-            registry.insert(root.clone(), m);
+            if Path::new("output.csv").exists() {
+                info!("Incremental scan detected");
+                load_registry("output.csv", &mut registry)?;
+
+                // Add root dir in case it's not known from previous scans
+                if !registry.contains_key(&root) {
+                    // Create ResourceMetadata and insert into the registry
+                    let p = Path::new(&root);
+                    let m = ResourceMetadata::new(&root, p.is_dir(), p.is_symlink(), 0, 0, false);
+                    registry.insert(root.clone(), m);
+                }
+
+                info!("Registry loaded with {} resources", add_groupings_usize(registry.len()));
+                scanner.incremental_scan(&root, &mut registry, &mut visitors);
+            } else {
+                info!("Starting full resource scan");
+                scanner.full_scan(&mut registry, &root, &mut visitors);
+                info!("Finished full resource scan elapsed time = {:?}", start_time.elapsed());
+            }
+            info!("Change Stats : ");
+            info!("added files   = {}", scanner.added_files());
+            info!("added dirs    = {}", scanner.added_dirs());
+            info!("updated files = {}", scanner.updated_files());
+            info!("updated dirs  = {}", scanner.updated_dirs());
+            info!("deleted files = {}", scanner.deleted_files());
+            info!("deleted dirs  = {}", scanner.deleted_dirs());
+            info!("elapsed time  = {:?}", start_time.elapsed());
+
+            save_registry(&mut registry)?;
+
+            for visitable_instance in &mut visitors {
+                visitable_instance.recap();
+            }
+        },
+        _ => {
+            let help = TurboTaskerApp::command().render_long_help();
+            println!("{help}");
+            exit(1);
         }
-
-        info!("Registry loaded with {} resources", add_groupings_usize(registry.len()));
-        scanner.incremental_scan(&root, &mut registry, &mut visitors);
-    } else {
-        info!("Starting full resource scan");
-        scanner.full_scan(&mut registry, &root, &mut visitors);
-        info!("Finished full resource scan elapsed time = {:?}", start_time.elapsed());
-    }
-    info!("Change Stats : ");
-    info!("added files   = {}", scanner.added_files());
-    info!("added dirs    = {}", scanner.added_dirs());
-    info!("updated files = {}", scanner.updated_files());
-    info!("updated dirs  = {}", scanner.updated_dirs());
-    info!("deleted files = {}", scanner.deleted_files());
-    info!("deleted dirs  = {}", scanner.deleted_dirs());
-    info!("elapsed time  = {:?}", start_time.elapsed());
-
-    save_registry(&mut registry)?;
-
-    for visitable_instance in &mut visitors {
-        visitable_instance.recap();
     }
 
     Ok(())
-}
-
-fn process_args() -> Result<String, Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect::<Vec<_>>();
-
-    if args.len() < 2 {
-        error!("Please provide a directory path.");
-        return Err(Box::new(io::Error::new(io::ErrorKind::InvalidInput, "Missing directory path")));
-    }
-
-    let r = args[1].clone();
-    Ok(r)
 }
 
 fn save_registry(registry: &mut HashMap<String, ResourceMetadata>) -> Result<(), std::io::Error> {
@@ -161,7 +163,7 @@ fn load_registry(file_path: &str, registry: &mut HashMap<String, ResourceMetadat
         let is_symlink: bool = record.get(2).ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Missing is_symlink in CSV record"))?.parse()?;
         // Assuming the system_time_from_string function parses the time correctly
         let modified_time = record.get(3).ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Missing modified_time in CSV record"))?.parse::<i64>()?;
-        let size_bytes : u64 = record.get(4).ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Missing size in CSV record"))?.parse::<u64>()?;
+        let size_bytes: u64 = record.get(4).ok_or_else(|| io::Error::new(io::ErrorKind::InvalidData, "Missing size in CSV record"))?.parse::<u64>()?;
 
         // Create ResourceMetadata and insert into the registry
         let resource_metadata = ResourceMetadata::new(&path, is_dir, is_symlink, modified_time, size_bytes, false);
---
+++ src/turbo_tasker_cli_config.rs
@@ -0,0 +1,20 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "tt", version = "0.1.0", author = "toaler", about = "Turbo Tasker - Keeping PC's organized since 2024!")]
+pub struct TurboTaskerApp {
+    #[command(subcommand)]
+    pub(crate) cmd: Option<Command>
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Command {
+    Disk {
+        #[arg(long = "duplicate_detection", required = false, short = 'd', help = "enable file duplicate detection")]
+        duplicate_detection: bool,
+
+        #[arg(long = "root", short = 'r', required = true, help = "root path to start resource analysis")]
+        root: String
+    },
+    Cpu,
+}
